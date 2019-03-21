12e54bc86e59b1602be2ecef67be637632f31508
Fabian <itsfabes@gmail.com>
2019-03-21 10:34:38 -0700

    Changed Port to 3001 Modified Title/Subtitles

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,7 +3,9 @@ import React from "react";
 const Titles = () => (
 	<div>
 		<h1 className="title-container__title">The Weather</h1>
-		<h3 className="title-container__subtitle">Know the temp. See conditions. Be prepared</h3>
+		<h3 className="title-container__subtitle">Know the temp</h3>
+		<h3 className="title-container__subtitle">See conditions</h3>
+		<h3 className="title-container__subtitle">Be prepared</h3>
 	</div>
 );
 
