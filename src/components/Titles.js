d2bbe811cb80f7fbfd63e4e83dd3817d9fb1ec55
Fabian <itsfabes@gmail.com>
2019-04-11 13:23:36 -0700

    Updated my own CSS touches

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Titles = () => (
 	<div>
-		<h1 className="title-container__title">The Weather</h1>
-		<h3 className="title-container__subtitle">Know the temp</h3>
-		<h3 className="title-container__subtitle">See conditions</h3>
+		<h1 className="title-container__title">WEATHER</h1>
+		<h3 className="title-container__subtitle">Get the temp</h3>
+		<h3 className="title-container__subtitle">Know the conditions</h3>
 		<h3 className="title-container__subtitle">Be prepared</h3>
 	</div>
 );
